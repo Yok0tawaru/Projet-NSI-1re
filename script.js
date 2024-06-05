f8577a43210296bf737fafafc73d929a4aa7d747
LeNuageDePluie <77206320+LeNuageDePluie@users.noreply.github.com>
2024-06-05 11:16:08 +0200

    Update script.js
    Ajout des descriptions dans le js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+// Fonction pour afficher le menu burger
 function toggleMenu() {
     var menu = document.querySelector('.menu');
     menu.classList.toggle('active');
 }
+
+// Fonction pour les métiers
